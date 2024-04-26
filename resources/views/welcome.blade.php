c892503480a3d6c3ec67335313e21ead6f9c69c5
Younes-Belaabda <belaabda.youness@gmail.com>
2024-04-26 15:47:08 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
     <script>
         function onScanSuccess(decodedText, decodedResult) {
             // handle the scanned code as you like, for example:
+            alert(`Code matched = ${decodedText}`, decodedResult);
             console.log(`Code matched = ${decodedText}`, decodedResult);
         }
 
