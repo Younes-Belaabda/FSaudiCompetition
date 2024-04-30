4a3657c7150a82fea1f99ce78f123400d8d467b6
Younes-Belaabda <belaabda.youness@gmail.com>
2024-04-30 13:29:46 +0100

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,13 +28,13 @@
             <!-- Brand Logo -->
             <div class="logo-box">
                 <!-- Brand Logo Light -->
-                <a href="/" class="logo-light">
+                <a href="{{ route('dashboard') }}" class="logo-light">
                     <img src="{{ asset('assets/guest/logo.png') }}" alt="logo" class="logo-lg" height="28">
                     <img src="{{ asset('assets/guest/logo.png') }}" alt="small logo" class="logo-sm" height="28">
                 </a>
 
                 <!-- Brand Logo Dark -->
-                <a href="/" class="logo-dark">
+                <a href="{{ route('dashboard') }}" class="logo-dark">
                     <img src="{{ asset('assets/guest/logo.png') }}" alt="dark logo" class="logo-lg" height="28">
                     <img src="{{ asset('assets/guest/logo.png') }}" alt="small logo" class="logo-sm" height="28">
                 </a>
@@ -97,15 +97,15 @@
                         <!-- Brand Logo -->
                         <div class="logo-box">
                             <!-- Brand Logo Light -->
-                            <a href="index.html" class="logo-light">
+                            <a href="{{ route('dashboard') }}" class="logo-light">
                                 <img src="{{ asset('assets/guest/logo.png') }}" alt="logo" class="logo-lg" height="22">
                                 <img src="{{ asset('assets/guest/logo.png') }}" alt="small logo" class="logo-sm" height="22">
                             </a>
 
                             <!-- Brand Logo Dark -->
-                            <a href="index.html" class="logo-dark">
-                                <img src="assets/images/logo-dark.png" alt="dark logo" class="logo-lg" height="22">
-                                <img src="assets/images/logo-sm.png" alt="small logo" class="logo-sm" height="22">
+                            <a href="{{ route('dashboard') }}" class="logo-dark">
+                                <img src="{{ asset('assets/guest/logo.png') }}" alt="dark logo" class="logo-lg" height="22">
+                                <img src="{{ asset('assets/guest/logo.png') }}" alt="small logo" class="logo-sm" height="22">
                             </a>
                         </div>
 
